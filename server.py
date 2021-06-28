9af24e8651a691488bccd6d0b355100315d7d2bc
gunzi <kento.sasaki.3a@stu.hosei.ac.jp>
2021-06-28 17:56:41 +0900

    hellohello

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+#Change!!!??
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', \
+    title="姓名判断bot", \
+    message="あなたのお名前は？")
+
+@app.route('/', methods=['POST'])
+def form():
+    field = request.form['field']
+    print(len(field))
+    if len(field) > 5:
+        field = '大吉'
+    else: field = '凶'
+    return render_template('index.html', \
+    title="姓名判断bot", \
+    message="あなたの運勢は「%s」です！" % field)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost', port=12345)
